0e018600927b9ed219510de8f1e15b4d6052fc75
Florian Feucht <v66r@users.sourceforge.net>
2012-04-07 13:21:52 +0200

    removed cfg.bat from being versioned

--- config/cfg.bat
+++
@@ -1,7 +0,0 @@
-set DESCENTDRIVE=/
-set DESCENTPATH=/home/vlo/.d2x-rebirth/data
-set DESCENTSTART=D2.BAT
-set MISSIONPATH=/home/vlo/.d2x-rebirth/data/missions
-set DEVILDRIVE=.
-set DEVILPATH=.
-set GRX20DRV=VESA
